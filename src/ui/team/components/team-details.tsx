51333dcfe943cad04b752479d0d6d5888da94c4a
burnspbrian <brian.burns@unbounce.com>
2021-02-17 21:32:37 -0800

    Refactor team name

--- src/ui/team/components/team-details.tsx
+++ src/ui/team/components/team-details.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useSelectedGolfers } from '../hooks/useSelectedGolfers';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { selectedGolfersState, teamNameState } from '../state/selectors';
 import { TeamList } from './team-list';
 import { TeamName } from './team-name';
+import { EditName } from './edit-name';
 
 export const TeamDetails = () => {
   const selectedGolfers = useRecoilValue(selectedGolfersState);
   const { removeGolfer } = useSelectedGolfers();
   const [teamName, setTeamName] = useRecoilState(teamNameState);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleNameChange = (newName: string) => {
+    setTeamName(newName);
+    setEditMode(false);
+  };
 
   return (
     <>
-      <TeamName teamName={teamName} setTeamName={setTeamName} />
+      {editMode ? (
+        <EditName teamName={teamName} handleNameChange={handleNameChange} />
+      ) : (
+        <TeamName teamName={teamName} setEditMode={setEditMode} />
+      )}
       <TeamList selectedGolfers={selectedGolfers} removeGolfer={removeGolfer} />
     </>
   );
--- src/ui/team/components/team-name.tsx
+++ src/ui/team/components/team-name.tsx
@@ -1,43 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import TextField from '@material-ui/core/TextField';
+
 import EditIcon from '@material-ui/icons/Edit';
 
 interface Props {
   teamName: string;
-  setTeamName: (name: string) => void;
+  setEditMode: (isEditMode: boolean) => void;
 }
 
 const StyledName = styled.div``;
 
-export const TeamName = ({ teamName, setTeamName }: Props) => {
-  const [editMode, setEditMode] = useState(false);
-  const [tempName, setTempName] = useState(teamName);
-
-  const handleNameChange = (newName: string) => {
-    setTeamName(newName);
-    setEditMode(false);
-  };
-
+export const TeamName = ({ teamName, setEditMode }: Props) => {
   return (
     <>
-      {editMode ? (
-        <TextField
-          id="standard"
-          label="Team Name"
-          defaultValue={teamName}
-          onBlur={(e) => handleNameChange(e.currentTarget.value)}
-          onChange={(e) => setTempName(e.currentTarget.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') handleNameChange(tempName);
-          }}
+      <StyledName>
+        {teamName}
+        <EditIcon
+          fontSize="small"
+          onClick={() => setEditMode(true)}
+          data-testid="edit-name-btn"
         />
-      ) : (
-        <StyledName>
-          {teamName}
-          <EditIcon fontSize="small" onClick={() => setEditMode(true)} />
-        </StyledName>
-      )}
+      </StyledName>
     </>
   );
 };
