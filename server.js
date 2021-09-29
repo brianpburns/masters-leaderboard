061d613684d3fdb810d148255b63712d5e729bed
Burns <brian.burns@unbounce.com>
2021-09-29 15:34:59 -0700

    Fix heroku

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (_req, res) => {
-  res.sendFile(path.resolve(path.join(__dirname, 'dist'), 'heroku-index.html'));
+  res.sendFile(
+    path.resolve(path.join(__dirname, 'public'), 'heroku-index.html')
+  );
 });
 
 const server = app.listen(process.env.PORT || 8080, function () {
