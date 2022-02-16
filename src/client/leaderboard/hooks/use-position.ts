5ed34701346493b868ccd7a4d92fea5c5bde1fef
Burns <brian.burns@unbounce.com>
2022-02-16 15:05:27 -0800

    Remove unnecessary hook

--- src/client/leaderboard/hooks/use-position.ts
+++
@@ -1,9 +0,0 @@
-import { useRecoilValue } from 'recoil';
-
-import { golfersState } from '../../app';
-
-export const usePosition = () => {
-  const golfers = useRecoilValue(golfersState);
-
-  return (golferId: number) => golfers[golferId].position;
-};
