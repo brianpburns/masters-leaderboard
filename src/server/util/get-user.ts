f9fdf82e12834bcbf2221254ce197be5ad6de248
Burns <brian.burns@unbounce.com>
2022-03-17 17:33:49 -0700

    Get handler for new team working

--- src/server/util/get-user.ts
+++ src/server/util/get-user.ts
@@ -1,13 +1,17 @@
 import fetch from 'node-fetch';
 
-export async function getGoogleUserId(accessToken: string) {
+export async function getGoogleUser(accessToken: string) {
   const res = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
     method: 'get',
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
   });
-  const { id }: { id: string } = await res.json();
+  const {
+    id,
+    name,
+    family_name,
+  }: { id: string; name: string; family_name: string } = await res.json();
 
-  return id;
+  return { id, name, familyName: family_name };
 }
