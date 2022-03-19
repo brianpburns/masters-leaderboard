abab9aecb380ed53260b33152068687843c33f91
Burns <brian.burns@unbounce.com>
2022-03-18 22:05:09 -0700

    Add login button

--- src/client/login/components/login.tsx
+++ src/client/login/components/login.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
-
-import { googleLoginUrl } from '../google-config';
-import { GoogleIcon } from '../icons';
-import {
-  LoginButtonContainer,
-  LoginContainer,
-  LoginIconWrapper,
-  StyledLoginButton,
-} from './styled';
+import { GoogleLogin } from 'react-google-login';
+import type {
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from 'react-google-login';
+import { googleConfig } from '../google-config';
+import { LoginButtonContainer, LoginContainer } from './styled';
+import { useSetRecoilState } from 'recoil';
+import { tokenState } from '../state/atoms';
+import { useGoogleAuthLogin } from 'src/client/api/hooks/use-google-auth-login';
 
 export const Login = () => {
+  const setToken = useSetRecoilState(tokenState);
+  const googleLogin = useGoogleAuthLogin();
+
+  const responseGoogle = (
+    response: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => {
+    if ('tokenId' in response) {
+      setToken(response.tokenId);
+    }
+  };
+
+  // TODO: Implement failure flow
+
   return (
     <LoginContainer>
       <LoginButtonContainer>
-        <StyledLoginButton
-          href={googleLoginUrl}
-          underline='none'
-          data-testid='login-button'
-        >
-          <LoginIconWrapper>
-            <GoogleIcon />
-          </LoginIconWrapper>
-          <span>Login with Google</span>
-        </StyledLoginButton>
+        <GoogleLogin
+          clientId={googleConfig.clientId}
+          buttonText='Login'
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
+        <button onClick={googleLogin}>Google Auth Test</button>
       </LoginButtonContainer>
     </LoginContainer>
   );
--- src/client/login/components/styled.tsx
+++ src/client/login/components/styled.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@material-ui/core';
 import styled from 'styled-components';
 
 export const LoginContainer = styled.div`
@@ -12,34 +11,4 @@ export const LoginContainer = styled.div`
 export const LoginButtonContainer = styled.div`
   margin: auto;
   height: 50px;
-`;
-
-export const StyledLoginButton = styled(Link)`
-  background-color: #fff;
-  display: inline-flex;
-  align-items: center;
-  color: #fff;
-  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.24), 0 0 1px 0 rgba(0, 0, 0, 0.24);
-  padding: 10px;
-  border-radius: 2px;
-  border: 1px solid transparent;
-  font-size: 14px;
-  font-weight: 500;
-  font-family: Roboto, sans-serif;
-
-  &:hover {
-    cursor: pointer;
-    opacity: 0.9;
-  }
-
-  &:active {
-    background-color: #eee;
-    color: rgba(0, 0, 0, 0.54);
-    opacity: 1;
-  }
-`;
-
-export const LoginIconWrapper = styled.div`
-  margin-right: 10px;
-  border-radius: 2px;
 `;
