d45f16d6202f36b7fde635172bee25b778785425
Burns <brian.burns@unbounce.com>
2022-03-19 19:57:38 -0700

    Fix mock data and handlers

--- test/mocks/index.ts
+++ test/mocks/index.ts
@@ -1,3 +1,4 @@
 export { setupMockServer } from './helpers/mock-server';
-export { golfersStateData } from './handler-data/golfers-state-data';
-export { teams } from './handler-data/teams';
+export { golfersStateData } from './data/golfers-state-data';
+export { teams } from './data/teams';
+export { cleanGolfers } from './data/clean-golfers';
