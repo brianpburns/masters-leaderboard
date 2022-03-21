6ceeaacd66444dc63915519c6d7d6535402f7c5a
Burns <brian.burns@unbounce.com>
2022-03-21 14:22:51 -0700

    Fix div nested in table error

--- src/client/leaderboard/components/body-container.tsx
+++ src/client/leaderboard/components/body-container.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useLoadTeams } from 'src/client/api';
-import { Loader } from 'src/client/shared';
 import { TeamType } from '../../../types';
 import { useAddPrizeMoney } from '../hooks/use-add-prize-money';
 import { TableBody } from './table-body';
@@ -9,14 +7,11 @@ const prizeMoneySortKey = (a: TeamType, b: TeamType) =>
   (a.prizeMoney || 0) < (b.prizeMoney || 0) ? 1 : -1;
 
 export const BodyContainer = () => {
-  // Needs to be done here to pull in any team changes
-  const { loading } = useLoadTeams();
   const teamsWithPrizeMoney = useAddPrizeMoney();
   const rankedTeams = teamsWithPrizeMoney.slice().sort(prizeMoneySortKey);
 
   return (
     <>
-      <Loader open={loading} />
       <TableBody tableData={rankedTeams} />
     </>
   );
