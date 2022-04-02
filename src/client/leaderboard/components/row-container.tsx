38a1c07456fbefb4e3ab2a7d2e33c304aca95346
Burns <brian.burns@unbounce.com>
2022-04-02 12:58:39 -0700

    Add click alert

--- src/client/leaderboard/components/row-container.tsx
+++ src/client/leaderboard/components/row-container.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { golfersState } from 'src/client/api';
+import { useSendAlert } from 'src/client/shared';
 import { Team } from '../../../types';
 import { useSortedGolfers } from '../hooks/use-sorted-golfers';
 import { PrimaryRow } from './primary-row';
@@ -15,9 +16,14 @@ export const RowContainer = ({ position, row }: Props) => {
   const [open, setOpen] = useState(false);
   const rankedGolfers = useSortedGolfers(row);
   const golfers = useRecoilValue(golfersState);
+  const sendAlert = useSendAlert();
 
   const toggleDropdown = (value: boolean) => {
-    if (golfers) setOpen(value);
+    if (golfers) {
+      setOpen(value);
+    } else {
+      sendAlert('Teams cannot be viewed until the tournament starts', 'info');
+    }
   };
 
   return (
