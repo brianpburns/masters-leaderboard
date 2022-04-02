800e695eab3aeaaa4851171901caba5351ea752f
Burns <brian.burns@unbounce.com>
2022-04-02 12:55:29 -0700

    Don't show dropdown if no data

--- src/client/leaderboard/components/row-container.tsx
+++ src/client/leaderboard/components/row-container.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { golfersState } from 'src/client/api';
 import { Team } from '../../../types';
 import { useSortedGolfers } from '../hooks/use-sorted-golfers';
 import { PrimaryRow } from './primary-row';
@@ -12,10 +14,20 @@ interface Props {
 export const RowContainer = ({ position, row }: Props) => {
   const [open, setOpen] = useState(false);
   const rankedGolfers = useSortedGolfers(row);
+  const golfers = useRecoilValue(golfersState);
+
+  const toggleDropdown = (value: boolean) => {
+    if (golfers) setOpen(value);
+  };
 
   return (
     <>
-      <PrimaryRow open={open} setOpen={setOpen} position={position} row={row} />
+      <PrimaryRow
+        open={open}
+        setOpen={toggleDropdown}
+        position={position}
+        row={row}
+      />
       <SubTable isOpen={open} rankedGolfers={rankedGolfers} />
     </>
   );
