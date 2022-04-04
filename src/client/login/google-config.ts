50cb6cb0c44569cc25510df3f1b72178827f9da6
Burns <brian.burns@unbounce.com>
2022-04-03 21:05:21 -0700

    Update client ID

--- src/client/login/google-config.ts
+++ src/client/login/google-config.ts
@@ -1,4 +1,4 @@
 export const googleConfig = {
   clientId:
-    '723926103233-6roqo85sa1glvu8umr5248rlbgi6nu6l.apps.googleusercontent.com', // new Masters credentials
+    '723926103233-6roqo85sa1glvu8umr5248rlbgi6nu6l.apps.googleusercontent.com',
 };
