e3f9af9c914920dc64a8b0177d172909cf5b3ea6
Burns <brian.burns@unbounce.com>
2022-04-03 21:27:06 -0700

    Update google config

--- src/client/login/google-config.ts
+++ src/client/login/google-config.ts
@@ -1,4 +1,4 @@
 export const googleConfig = {
   clientId:
-    '723926103233-6roqo85sa1glvu8umr5248rlbgi6nu6l.apps.googleusercontent.com',
+    '723926103233-frdd95rbqd60l3hi78m7mqhln45prppg.apps.googleusercontent.com',
 };
