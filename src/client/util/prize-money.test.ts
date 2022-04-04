ef100327d4a7f55605015386d64b65b7a2348330
Burns <brian.burns@unbounce.com>
2022-04-03 17:46:58 -0700

    Refactor and test coverage

--- src/client/util/prize-money.test.ts
+++ src/client/util/prize-money.test.ts
@@ -1,31 +1,62 @@
-import { GolferData } from '../../types';
+import { addPrizeMoney, getPrizeMoney } from './prize-money';
 
-const player1: GolferData = {
-  id: 0,
-  name: 'Rory McIlroy',
-  position: 1,
-  topar: -5,
-  thru: '10',
-  today: '-5',
-  teetime: '11:00',
+const mockRankings = {
+  0: {
+    golfers: [1717, 27129],
+    prizeMoney: 0,
+    topar: 0,
+  },
+  1: {
+    golfers: [28237, 33204],
+    prizeMoney: 0,
+    topar: -2,
+  },
+  10: {
+    golfers: [52215],
+    prizeMoney: 0,
+    topar: 1,
+  },
+  70: {
+    golfers: [26851],
+    prizeMoney: 0,
+    topar: 10,
+  },
 };
 
-const player2: GolferData = {
-  id: 1,
-  name: 'Justin Rose',
-  position: 100,
-  topar: 5,
-  thru: '10',
-  today: '+5',
-  teetime: '11:00',
-};
+describe('getPrizeMoney', () => {
+  test('handles players who miss the cut', () => {
+    const prizeMoney = getPrizeMoney('0', mockRankings[0].golfers, '1000');
 
-const playersStats: GolferData[] = [player1, player2];
+    expect(prizeMoney).toEqual(10000);
+  });
 
-describe('calculatePrizeMoney', () => {
-  test.todo('returns correct data');
-});
+  test('handles players outside the cut in the first round', () => {
+    const prizeMoney = getPrizeMoney('70', mockRankings[70].golfers, '1000');
 
-describe('playerPrizeMoney', () => {
-  test.todo('returns correct data');
+    expect(prizeMoney).toEqual(10000);
+  });
+
+  test('handles players outside the cut in the second round', () => {
+    const prizeMoney = getPrizeMoney('70', mockRankings[70].golfers, '0100');
+
+    expect(prizeMoney).toEqual(10000);
+  });
+
+  test('handles players who make the cut but finish outside the top 50', () => {
+    const prizeMoney = getPrizeMoney('70', mockRankings[70].golfers, '0010');
+
+    expect(prizeMoney).toEqual(25000);
+  });
+
+  test('handles splitting money on a tie', () => {
+    const prizeMoney = getPrizeMoney('1', mockRankings[1].golfers, '0010');
+
+    expect(prizeMoney).toEqual(1656000);
+  });
+
+  test('handles player alone in position', () => {
+    const prizeMoney = getPrizeMoney('10', mockRankings[10].golfers, '0010');
+
+    expect(prizeMoney).toEqual(310500);
+  });
 });
