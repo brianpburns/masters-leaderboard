67cbec15e9ee44fa76bcc7e6659dcfc264506862
Burns <brian.burns@unbounce.com>
2022-04-03 17:57:32 -0700

    Add coverage for checkboxes

--- src/client/team/components/available-golfers-list.test.tsx
+++ src/client/team/components/available-golfers-list.test.tsx
@@ -19,16 +19,28 @@ const mockInvitees = [
     top10: false,
   },
   {
-    last_name: 'Couples ',
-    first_name: 'Fred',
-    id: '1226',
-    countryName: 'United States',
-    countryCode: 'USA',
+    last_name: 'Power ',
+    first_name: 'Séamus',
+    id: '28252',
+    countryName: 'Ireland',
+    countryCode: 'IRL',
+    Amateur: '',
+    First: '1',
+    Past: '',
+    image: false,
+    top10: false,
+  },
+  {
+    last_name: 'McIlroy ',
+    first_name: 'Rory',
+    id: '28237',
+    countryName: 'N. Ireland',
+    countryCode: 'NIR',
     Amateur: '',
     First: '',
-    Past: '1',
+    Past: '',
     image: true,
-    top10: false,
+    top10: true,
   },
 ];
 
@@ -65,13 +77,64 @@ describe('Available Golfers List', () => {
   test('filters list of golfers based on searchTerm', () => {
     renderGolfersList();
 
-    expect(screen.getByText('Fred Couples')).toBeTruthy();
+    expect(screen.getByText('Rory McIlroy')).toBeTruthy();
 
     const searchBarInput = screen.getByTestId('search-bar-input');
     userEvent.click(searchBarInput);
     userEvent.type(searchBarInput, 'Tiger');
 
     expect(screen.getByText('Tiger Woods')).toBeTruthy();
-    expect(screen.queryByText('Fred Couples')).toBeFalsy();
+    expect(screen.queryByText('Rory McIlroy')).toBeFalsy();
+  });
+
+  test('checkbox toggles rookies', () => {
+    renderGolfersList();
+
+    const rookiesCheckbox = screen.getByLabelText('Rookies');
+    userEvent.click(rookiesCheckbox);
+
+    expect(screen.getByText('Séamus Power')).toBeTruthy();
+    expect(screen.queryByText('Tiger Woods')).toBeFalsy();
+    expect(screen.queryByText('Rory McIlroy')).toBeFalsy();
+
+    userEvent.click(rookiesCheckbox);
+
+    expect(screen.getByText('Séamus Power')).toBeTruthy();
+    expect(screen.getByText('Tiger Woods')).toBeTruthy();
+    expect(screen.getByText('Rory McIlroy')).toBeTruthy();
+  });
+
+  test('checkbox toggles top 10', () => {
+    renderGolfersList();
+
+    const rookiesCheckbox = screen.getByLabelText('Top 10');
+    userEvent.click(rookiesCheckbox);
+
+    expect(screen.getByText('Rory McIlroy')).toBeTruthy();
+    expect(screen.queryByText('Tiger Woods')).toBeFalsy();
+    expect(screen.queryByText('Séamus Power')).toBeFalsy();
+
+    userEvent.click(rookiesCheckbox);
+
+    expect(screen.getByText('Séamus Power')).toBeTruthy();
+    expect(screen.getByText('Tiger Woods')).toBeTruthy();
+    expect(screen.getByText('Rory McIlroy')).toBeTruthy();
+  });
+
+  test('checkbox toggles other', () => {
+    renderGolfersList();
+
+    const rookiesCheckbox = screen.getByLabelText('Other');
+    userEvent.click(rookiesCheckbox);
+
+    expect(screen.getByText('Tiger Woods')).toBeTruthy();
+    expect(screen.queryByText('Rory McIlroy')).toBeFalsy();
+    expect(screen.queryByText('Séamus Power')).toBeFalsy();
+
+    userEvent.click(rookiesCheckbox);
+
+    expect(screen.getByText('Séamus Power')).toBeTruthy();
+    expect(screen.getByText('Tiger Woods')).toBeTruthy();
+    expect(screen.getByText('Rory McIlroy')).toBeTruthy();
   });
 });
