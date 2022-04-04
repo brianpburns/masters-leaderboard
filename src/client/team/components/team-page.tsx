f20f5de8c13d672cf0d3dd8d5bd970e9c40280d3
Burns <brian.burns@unbounce.com>
2022-04-03 19:45:58 -0700

    Add error handling for Google signin

--- src/client/team/components/team-page.tsx
+++ src/client/team/components/team-page.tsx
@@ -8,13 +8,16 @@ import { TeamPageContainer } from './styled';
 import { TeamContent } from './team-content';
 
 export const TeamPage = () => {
-  const { token, loaded, signIn } = useGoogleSignIn(true);
+  const { token, loaded, signIn, error } = useGoogleSignIn(true);
   const selectionPhase = useRecoilValue(selectionPhaseState);
+
+  const signInSuccess = loaded && !error;
 
   return (
     <TeamPageContainer>
-      <Loader open={!loaded} />
-      {loaded &&
+      <Loader open={!loaded && !error} />
+      {error && <p>Oops there was an error loading the page</p>}
+      {signInSuccess &&
         (token ? (
           <TeamContent selectionPhase={selectionPhase} />
         ) : (
