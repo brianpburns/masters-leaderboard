bbda9023822dbb3525c101dbcb60fca58828c20d
Burns <brian.burns@unbounce.com>
2022-04-07 00:15:03 -0700

    Add player type to leaderboar

--- src/client/leaderboard/components/styled.tsx
+++ src/client/leaderboard/components/styled.tsx
@@ -4,3 +4,11 @@ import styled from 'styled-components';
 export const StyledTableContainer = styled(TableContainer)`
   background-color: white;
 `;
+
+export const StyledPlayerType = styled.span<{
+  top10: boolean;
+  amateur: boolean;
+}>`
+  color: ${({ top10, amateur }) =>
+    top10 ? '#e9c70d' : amateur ? 'orange' : 'darkgreen'};
+`;
--- src/client/leaderboard/components/sub-row.tsx
+++ src/client/leaderboard/components/sub-row.tsx
@@ -1,22 +1,37 @@
 import { TableCell, TableRow } from '@mui/material';
 import React from 'react';
+import { useGetGolferData } from 'src/client/data/hooks/use-get-golfer-data';
 import { GolferData } from 'src/types';
 import { displayToPar } from '../utils/display-to-par';
+import { StyledPlayerType } from './styled';
 
 interface Props {
   golfer: GolferData;
   prizeMoney: string;
 }
 
-export const SubRow = ({ golfer, prizeMoney }: Props) => (
-  <TableRow key={golfer.id}>
-    <TableCell component='th' scope='row'>
-      {golfer.position === 0 ? '-' : golfer.position}
-    </TableCell>
-    <TableCell>{golfer.name}</TableCell>
-    <TableCell>{displayToPar(golfer.topar)}</TableCell>
-    <TableCell>{golfer.thru}</TableCell>
-    <TableCell>{golfer.today}</TableCell>
-    <TableCell>{prizeMoney}</TableCell>
-  </TableRow>
-);
+export const SubRow = ({ golfer, prizeMoney }: Props) => {
+  const { id, name, position, topar, today, thru } = golfer;
+  const { getGolferData } = useGetGolferData();
+  const { top10, First, Amateur } = getGolferData(id);
+  const rookie = First === '1';
+  const amateur = Amateur === '1';
+
+  return (
+    <TableRow key={id}>
+      <TableCell component='th' scope='row'>
+        {position === 0 ? '-' : position}
+      </TableCell>
+      <TableCell>
+        {name}{' '}
+        <StyledPlayerType top10={top10} amateur={amateur}>
+          {top10 ? '(10)' : amateur ? '(A)' : rookie && '(R)'}
+        </StyledPlayerType>
+      </TableCell>
+      <TableCell>{displayToPar(topar)}</TableCell>
+      <TableCell>{thru}</TableCell>
+      <TableCell>{today}</TableCell>
+      <TableCell>{prizeMoney}</TableCell>
+    </TableRow>
+  );
+};
