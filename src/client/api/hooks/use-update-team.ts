1fb4bfd3641a6e6860e6ff6d009d06ec566f7423
Burns <brian.burns@unbounce.com>
2022-08-17 15:47:23 -0700

    Cleanup

--- src/client/api/hooks/use-update-team.ts
+++ src/client/api/hooks/use-update-team.ts
@@ -3,6 +3,14 @@ import { useAuthToken } from 'src/client/store';
 import { Team } from '../../../types';
 import { updateTeam } from '../fetch/update-team';
 
+const generateMessage = (picksRemaining: number) => {
+  if (picksRemaining > 0) {
+    return ` ${picksRemaining} pick${picksRemaining > 1 ? 's' : ''} left.`;
+  }
+
+  return '';
+};
+
 export const useUpdateTeam = () => {
   const { authToken } = useAuthToken();
   const sendAlert = useSendAlert();
@@ -10,11 +18,8 @@ export const useUpdateTeam = () => {
   const updateTeamDetails = async (team: Team) => {
     try {
       await updateTeam(team, authToken);
-      const picksRemaining = 10 - team.golfer_ids.length;
-      const picksMessage =
-        picksRemaining > 0
-          ? ` ${picksRemaining} pick${picksRemaining > 1 ? 's' : ''} left.`
-          : '';
+      const picksMessage = generateMessage(10 - team.golfer_ids.length);
+
       sendAlert(`Save Success. ${picksMessage}`, 'success');
     } catch (err) {
       if (err instanceof Error) {
