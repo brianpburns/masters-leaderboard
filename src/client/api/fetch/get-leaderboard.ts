51ddb8fb9f76a1702e672a19a7704ffa0282c242
Burns <brian.burns@unbounce.com>
2023-03-26 14:16:45 -0700

    Update leaderboard link

--- src/client/api/fetch/get-leaderboard.ts
+++ src/client/api/fetch/get-leaderboard.ts
@@ -1,5 +1,5 @@
 const LEADERBOARD_URL =
-  'https://www.masters.com/en_US/scores/feeds/2022/scores.json';
+  'https://www.masters.com/en_US/scores/feeds/2023/scores.json';
 
 export const getLeaderboard = async () => fetch(LEADERBOARD_URL);
 
