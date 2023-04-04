eaec3f1f149815ace9b28285997631887ae061a6
Burns <brian.burns@unbounce.com>
2023-04-03 22:51:01 -0700

    Avoid position errors on leaderboard in selection

--- src/client/features/leaderboard/components/row-container.tsx
+++ src/client/features/leaderboard/components/row-container.tsx
@@ -4,7 +4,6 @@ import { useSendAlert } from 'src/client/features/shared';
 import { selectPhaseSelection } from 'src/client/store';
 import { selectGolfersList } from 'src/client/store/global-slice/selectors';
 import { TeamWithPrizeMoney } from '../../../../types';
-import { useSortedGolfers } from '../hooks/use-sorted-golfers';
 import { PrimaryRow } from './primary-row';
 import { SubTable } from './sub-table';
 
@@ -15,7 +14,6 @@ interface Props {
 
 export const RowContainer = ({ position, row }: Props) => {
   const [open, setOpen] = useState(false);
-  const rankedGolfers = useSortedGolfers(row);
   const golfers = useSelector(selectGolfersList);
   const sendAlert = useSendAlert();
   const selectionPhase = useSelector(selectPhaseSelection);
@@ -41,9 +39,7 @@ export const RowContainer = ({ position, row }: Props) => {
         row={row}
         selectionPhase={selectionPhase}
       />
-      {displaySubTable && (
-        <SubTable isOpen={open} rankedGolfers={rankedGolfers} />
-      )}
+      {displaySubTable && <SubTable isOpen={open} row={row} />}
     </>
   );
 };
--- src/client/features/leaderboard/components/sub-table.tsx
+++ src/client/features/leaderboard/components/sub-table.tsx
@@ -7,35 +7,41 @@ import {
   TableRow,
 } from '@mui/material';
 import React from 'react';
+import { TeamWithPrizeMoney } from 'src/types';
+import { useSortedGolfers } from '../hooks/use-sorted-golfers';
 import { SubRowContainer } from './sub-row-container';
 
 interface Props {
   isOpen: boolean;
-  rankedGolfers: number[];
+  row: TeamWithPrizeMoney;
 }
 
-export const SubTable = ({ isOpen, rankedGolfers }: Props) => (
-  <TableRow>
-    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-      <Collapse in={isOpen} timeout='auto' unmountOnExit>
-        <Table size='small' aria-label='purchases' padding='none'>
-          <TableHead>
-            <TableRow data-testid='sub-table-row'>
-              <TableCell>Pos</TableCell>
-              <TableCell>Player</TableCell>
-              <TableCell>Total</TableCell>
-              <TableCell>Thru</TableCell>
-              <TableCell>Today</TableCell>
-              <TableCell>Money</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rankedGolfers.map((id: number) => (
-              <SubRowContainer key={id} golferId={id} />
-            ))}
-          </TableBody>
-        </Table>
-      </Collapse>
-    </TableCell>
-  </TableRow>
-);
+export const SubTable = ({ isOpen, row }: Props) => {
+  const rankedGolfers = useSortedGolfers(row);
+
+  return (
+    <TableRow>
+      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+        <Collapse in={isOpen} timeout='auto' unmountOnExit>
+          <Table size='small' aria-label='purchases' padding='none'>
+            <TableHead>
+              <TableRow data-testid='sub-table-row'>
+                <TableCell>Pos</TableCell>
+                <TableCell>Player</TableCell>
+                <TableCell>Total</TableCell>
+                <TableCell>Thru</TableCell>
+                <TableCell>Today</TableCell>
+                <TableCell>Money</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rankedGolfers.map((id: number) => (
+                <SubRowContainer key={id} golferId={id} />
+              ))}
+            </TableBody>
+          </Table>
+        </Collapse>
+      </TableCell>
+    </TableRow>
+  );
+};
