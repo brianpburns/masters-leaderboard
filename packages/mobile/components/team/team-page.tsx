d9bb429e1a13ee233c467a9d50edfe8fb2ca725b
Burns <brian.burns@unbounce.com>
2024-03-04 20:09:28 -0800

    Fix typing issue with list

--- packages/mobile/components/team/team-page.tsx
+++ packages/mobile/components/team/team-page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, ListRenderItem, StyleSheet, Text, View } from 'react-native';
 
-const golfers = [
+type Player = {
+  countryCode: string;
+  name: string;
+};
+
+const player: Player[] = [
   {
     countryCode: 'IRL',
     name: 'Rory McIlroy',
@@ -12,24 +17,24 @@ const golfers = [
   },
 ];
 
+const renderGolfer: ListRenderItem<Player> = ({ item }: { item: Player }) => {
+  return (
+    <View style={styles.listItem}>
+      <View style={styles.flagContainer}>
+        <Image
+          style={styles.flag}
+          source={{ uri: `https://www.masters.com/assets/images/flags/${item.countryCode}_sm.gif` }}
+        />
+      </View>
+      <Text style={styles.golferName}>{item.name}</Text>
+    </View>
+  );
+};
+
 export const TeamPage = () => {
   return (
     <View>
-      <FlatList
-        style={styles.listContainer}
-        data={golfers}
-        renderItem={({ item }) => (
-          <View style={styles.listItem}>
-            <View style={styles.flagContainer}>
-              <Image
-                style={styles.flag}
-                source={{ uri: `https://www.masters.com/assets/images/flags/${item.countryCode}_sm.gif` }}
-              />
-            </View>
-            <Text style={styles.golferName}>{item.name}</Text>
-          </View>
-        )}
-      />
+      <FlatList style={styles.listContainer} data={player} renderItem={renderGolfer} />
     </View>
   );
 };
