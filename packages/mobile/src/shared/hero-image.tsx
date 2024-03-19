563113ec08c9a5e29f8a5cbe0b199478b501f98e
Burns <brian.burns@unbounce.com>
2024-03-18 20:30:02 -0700

    Pull out hero image component

---
+++ packages/mobile/src/shared/hero-image.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
+
+export const thirteenthTeeBox =
+  'https://d9hhrg4mnvzow.cloudfront.net/unbouncepages.com/dadd721f-48ec-497c-974d-ce09c0297264/masters-13th-hole.jpeg';
+
+export const HeroImage = () => {
+  return <ImageBackground source={{ uri: thirteenthTeeBox }} resizeMode="cover" style={styles.heroImage} />;
+};
+
+const styles = StyleSheet.create({
+  heroImage: {
+    flex: 1,
+    alignSelf: 'stretch',
+    height: 150,
+  },
+});
