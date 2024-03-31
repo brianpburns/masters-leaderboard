5873bc50c7f86ef6be9925c1f98081a97ee7e52d
Burns <brian.burns@unbounce.com>
2024-03-31 11:59:42 -0700

    Show login when unauthenticated

--- packages/mobile/app/tabs/index.tsx
+++ packages/mobile/app/tabs/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-import { HeroImage } from 'src/shared/hero-image';
+import { useSelector } from 'react-redux';
+import { Login } from 'src/login/components/login';
+import { HeroImage } from 'src/shared/components/hero-image';
+import { selectAuthToken } from 'src/store';
 import { TeamPage } from '../../components/team';
 
 export const mastersLogoSrc =
   'http://d9hhrg4mnvzow.cloudfront.net/unbouncepages.com/augusta-masters-2019/d05d1861-masters-logo-masters-gimp_0ds03p0dq03p000000001.png';
 
 export default function TabOneScreen() {
+  const authToken = useSelector(selectAuthToken);
+
   return (
     <ScrollView>
       <HeroImage />
-      <TeamPage />
+      {authToken ? <TeamPage /> : <Login />}
     </ScrollView>
   );
 }
