433d162740a22aa4ccb2bb89c59bff40a173697a
Burns <brian.burns@unbounce.com>
2024-03-31 11:55:24 -0700

    Handle multiple OAuth clients

--- packages/web/src/server/util/verify-token.ts
+++ packages/web/src/server/util/verify-token.ts
@@ -2,11 +2,22 @@ import { OAuth2Client, TokenPayload } from 'google-auth-library';
 
 export const verifyToken = async (token: string) => {
   const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+  const webClientId = process.env.GOOGLE_WEB_CLIENT_ID as string;
+  const iosClientId = process.env.GOOGLE_IOS_CLIENT_ID as string;
 
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: process.env.GOOGLE_CLIENT_ID,
-  });
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: webClientId,
+    });
 
-  return ticket.getPayload() as TokenPayload;
+    return ticket.getPayload() as TokenPayload;
+  } catch {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: iosClientId,
+    });
+
+    return ticket.getPayload() as TokenPayload;
+  }
 };
