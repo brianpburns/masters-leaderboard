d66f4a39cebba5350d5629362772031613da0f1f
Burns <brian.burns@unbounce.com>
2024-03-31 11:55:41 -0700

    Move image

--- packages/mobile/app/tabs/two.tsx
+++ packages/mobile/app/tabs/two.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
 
-import { HeroImage } from 'src/shared/hero-image';
+import { HeroImage } from 'src/shared/components/hero-image';
 import Leaderboard from '../../components/edit-screen';
 
 export default function TabTwoScreen() {
