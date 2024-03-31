1e05d12ba49d34eaf60d93e3589e6e45b8833995
Burns <brian.burns@unbounce.com>
2024-03-31 11:59:15 -0700

    Add get team request

---
+++ packages/mobile/src/api/fetch/get-team.ts
@@ -0,0 +1,20 @@
+import { Team } from 'src/types';
+
+interface GetTeamResponse {
+  team: Team;
+  new_team: boolean;
+}
+
+export const getTeam = async (token: string) => {
+  const res = await fetch('http://localhost:8090/api/team', {
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  });
+
+  if (![200, 201].includes(res.status)) {
+    throw new Error(`Failed to get team. Server responded with a ${res.status} code`);
+  }
+
+  return (await res.json()) as GetTeamResponse;
+};
