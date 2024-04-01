874fd2b9e1f6032808acc314127a31165cd5a52f
Burns <brian.burns@unbounce.com>
2024-03-31 21:28:59 -0700

    Add selection limits

--- packages/mobile/src/team-page/components/golfers-list.tsx
+++ packages/mobile/src/team-page/components/golfers-list.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { FlatList, Image, ListRenderItem, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { FlatList, Image, ListRenderItem, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { top10Ids } from 'src/data/golfers-data';
+import { PressIcon } from 'src/shared/components/press-icon';
 import { useManageGolfers } from 'src/shared/hooks/use-manage-golfers';
 import { selectPhaseSelection } from 'src/store';
 import { Player } from 'src/types';
+import { useSelectionLimits } from '../hooks/use-selection-limits';
 
 interface Props {
   data: Player[];
@@ -17,6 +18,11 @@ interface ListItemProps {
   selectedView: boolean;
   addGolfer: (id: number) => void;
   removeGolfer: (id: number) => void;
+  disabled: {
+    all: boolean;
+    top10: boolean;
+    other: boolean;
+  };
 }
 
 const generateRenderList = ({
@@ -24,25 +30,37 @@ const generateRenderList = ({
   selectionPhase,
   addGolfer,
   removeGolfer,
+  disabled,
 }: ListItemProps): ListRenderItem<Player> => {
   return ({ item }: { item: Player }) => {
     const rookie = item.First === '1';
     const amateur = item.Amateur === '1';
     const top10 = top10Ids.includes(item.id);
+    const other = !rookie && !amateur && !top10;
+    const addDisabled = disabled.all || (top10 && disabled.top10) || (other && disabled.other);
 
-    const handlePress = () => {
-      if (selectedView) {
-        if (selectionPhase) {
-          console.log('remove golfer');
-          removeGolfer(parseInt(item.id));
-        }
-      } else {
-        addGolfer(parseInt(item.id));
-      }
-    };
+    const teamPickedMessage = 'Team full';
+    const rookieMessage = 'Must pick A/rookie';
+    const top10Text = disabled.other ? rookieMessage : disabled.top10 ? 'At top 10 limit' : 'Top 10';
+    const otherText = disabled.all ? teamPickedMessage : addDisabled ? rookieMessage : '';
+    const text = disabled.all ? teamPickedMessage : top10 ? top10Text : amateur ? 'A' : rookie ? 'R' : otherText;
+    const textStyle = top10 ? styles.top10 : amateur ? styles.amateur : rookie ? styles.rookie : styles.error;
 
     return (
-      <Pressable style={styles.listItem} onPress={handlePress}>
+      <View style={[styles.listItem, addDisabled ? styles.disabled : {}]}>
+        <View style={styles.iconsWrapper}>
+          {selectedView && selectionPhase && (
+            <PressIcon onPress={() => removeGolfer(parseInt(item.id))} name="minus-square-o" color="red" />
+          )}
+          {!selectedView && (
+            <PressIcon
+              onPress={() => addGolfer(parseInt(item.id))}
+              disabled={addDisabled}
+              name="plus-square-o"
+              color="green"
+            />
+          )}
+        </View>
         <View style={styles.flagContainer}>
           <Image
             style={styles.flag}
@@ -53,15 +71,10 @@ const generateRenderList = ({
 
         <View style={styles.iconsContainer}>
           <View style={styles.iconsWrapper}>
-            <Text style={top10 ? styles.top10 : amateur ? styles.amateur : rookie ? styles.rookie : {}}>
-              {' '}
-              {top10 ? '10' : amateur ? 'A' : rookie && 'R'}{' '}
-            </Text>
-            {selectedView && selectionPhase && <Icon name="minus" />}
-            {!selectedView && <Icon name="plus" size={15} />}
+            <Text style={[textStyle, addDisabled ? { color: 'red' } : {}]}>{text}</Text>
           </View>
         </View>
-      </Pressable>
+      </View>
     );
   };
 };
@@ -69,8 +82,9 @@ const generateRenderList = ({
 export const GolfersList = ({ data, selectedView }: Props) => {
   const selectionPhase = useSelector(selectPhaseSelection);
   const { addGolfer, removeGolfer } = useManageGolfers();
+  const disabled = useSelectionLimits(selectedView);
 
-  const renderGolfer = generateRenderList({ selectedView, selectionPhase, removeGolfer, addGolfer });
+  const renderGolfer = generateRenderList({ selectedView, selectionPhase, removeGolfer, addGolfer, disabled });
 
   return (
     <ScrollView>
@@ -86,7 +100,6 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 0,
     margin: 0,
-    marginTop: 10,
     marginBottom: 50,
   },
   listItem: {
@@ -119,12 +132,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   iconsWrapper: {
-    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
+    marginRight: 5,
   },
   top10: {
     fontWeight: 'bold',
-    color: 'gold',
+    color: 'green',
   },
   amateur: {
     fontWeight: 'bold',
@@ -132,6 +146,13 @@ const styles = StyleSheet.create({
   },
   rookie: {
     fontWeight: 'bold',
-    color: 'green',
+    color: 'blue',
+  },
+  error: {
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  disabled: {
+    backgroundColor: 'lightgrey',
   },
 });
