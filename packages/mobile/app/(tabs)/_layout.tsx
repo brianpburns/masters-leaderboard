5ff106e0ae8679bebcac25a4b5ce97c59ddb039c
Burns <brian.burns@unbounce.com>
2024-04-04 21:45:42 -0700

    Add icons

--- packages/mobile/app/(tabs)/_layout.tsx
+++ packages/mobile/app/(tabs)/_layout.tsx
@@ -1,20 +1,15 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { useColorScheme } from 'react-native';
+import AntDesignIcons from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import { useSelector } from 'react-redux';
 import { useInitializeState } from 'src/data/hooks/use-initialize-state';
 import { ProfileMenu } from 'src/header/profile-menu';
 import { selectPhaseSelection } from 'src/store';
 import { colors } from '../../constants/color';
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -31,7 +26,7 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Team',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <AntDesignIcons name="team" color={color} size={25} />,
           headerRight: () => <ProfileMenu />,
         }}
       />
@@ -40,7 +35,7 @@ export default function TabLayout() {
         name="players"
         options={{
           title: 'Players',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons name="body" color={color} size={25} />,
           headerRight: () => <ProfileMenu />,
         }}
       />
@@ -49,7 +44,7 @@ export default function TabLayout() {
         name="leaderboard"
         options={{
           title: 'Leaderboard',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons name="leaderboard" size={25} color={color} />,
           headerRight: () => <ProfileMenu />,
         }}
       />
