61e3af48581b425e4a26a47c865c0dafe9762842
Burns <brian.burns@unbounce.com>
2024-04-09 18:54:18 -0700

    Exclude top 10 from rookies

--- packages/mobile/src/team-page/hooks/use-filter.ts
+++ packages/mobile/src/team-page/hooks/use-filter.ts
@@ -13,7 +13,9 @@ export const useFilter = (players: Player[], searchTerm: string) => {
 
   useEffect(() => {
     if (filter === 'rookies') {
-      const rookies = players.filter((golfer) => golfer.First === '1' || golfer.Amateur === '1');
+      const rookies = players.filter(
+        (golfer) => !top10Ids.includes(golfer.id) && (golfer.First === '1' || golfer.Amateur === '1'),
+      );
       setFilteredResults(rookies);
     } else if (filter === 'top10') {
       const top10 = players.filter((golfer) => top10Ids.includes(golfer.id));
