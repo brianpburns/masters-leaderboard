d09c605edc848a55bbf02c41d01ba744420cf1a3
Burns <brian.burns@unbounce.com>
2024-04-09 20:02:52 -0700

    Update app images and base url

--- packages/mobile/src/api/config.ts
+++ packages/mobile/src/api/config.ts
@@ -1,4 +1,4 @@
-// export const baseUrl = 'https://masters-app.onrender.com';
+export const baseUrl = 'https://masters-app.onrender.com';
 // Android requires the IP address rather than localhost
-export const baseUrl = 'http://192.168.0.21:8090';
+// export const baseUrl = 'http://192.168.0.21:8090';
 // export const baseUrl = 'http://localhost:8090';
