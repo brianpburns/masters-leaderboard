21d77579d6abea038548ccb23c0cff267003b198
Brian Burns <brian.burns@unbounce.com>
2025-04-08 12:14:45 -0700

    Fix image (#72)

--- packages/web/src/client/data/image-srcs.ts
+++ packages/web/src/client/data/image-srcs.ts
@@ -2,6 +2,4 @@ export const mastersLogoSrc =
   'http://d9hhrg4mnvzow.cloudfront.net/unbouncepages.com/augusta-masters-2019/d05d1861-masters-logo-masters-gimp_0ds03p0dq03p000000001.png';
 
 export const thirteenthTeeBox =
-  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.masters.com%2Fen_US%2Fnews%2Finline%2F_c05e2d3d_b626.html&psig=AOvVaw2-n0rAyv6xje6fSMAJzhdB&ust=1744217118700000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCNDbqaPxyIwDFQAAAAAdAAAAABAE';
-
-export const twelfthBridge = '//d9hhrg4mnvzow.cloudfront.net/unbouncepages.com/masters-2022/9721323d-masters.jpg';
+  'https://res.cloudinary.com/duwovvtle/image/upload/v1744138192/masters/qsrnbhikg07psn3j1h32.avif';
