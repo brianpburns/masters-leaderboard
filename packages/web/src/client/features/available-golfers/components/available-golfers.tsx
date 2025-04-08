8a9b9f63833d2f0cf711b8118b1f34ba73cf2f9d
Burns <brian.burns@unbounce.com>
2025-04-08 10:34:01 -0700

    Remove duplicate hook call

--- packages/web/src/client/features/available-golfers/components/available-golfers.tsx
+++ packages/web/src/client/features/available-golfers/components/available-golfers.tsx
@@ -4,53 +4,38 @@ import { GolfersListItem, useManageGolfers } from '../../shared/';
 import { useFilter } from '../hooks/use-filter';
 import { Checkbox } from './checkbox';
 import { SearchBar } from './search-bar';
-import {
-  FiltersContainer,
-  GolfersListContainer,
-  RemainingPicks,
-  StyledGolfersList,
-} from './styled';
+import { FiltersContainer, GolfersListContainer, RemainingPicks, StyledGolfersList } from './styled';
 
 export const AvailableGolfersList = () => {
-  const { addGolfer } = useManageGolfers();
-  const { selectedGolfers } = useManageGolfers();
+  const { addGolfer, selectedGolfers } = useManageGolfers();
   const { searchTerm, setSearchTerm, searchResults } = useGetGolferData();
   const { filter, setFilter, results } = useFilter(searchResults, searchTerm);
   const remainingPicks = 10 - selectedGolfers.length;
 
   return (
-    <GolfersListContainer data-testid='golfers-list'>
+    <GolfersListContainer data-testid="golfers-list">
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <FiltersContainer>
         <Checkbox
-          label='Top 10'
+          label="Top 10"
           checked={filter === 'top10'}
           onClick={(checked) => setFilter(checked ? 'top10' : 'none')}
         />
         <Checkbox
-          label='(A)/Rookie'
+          label="(A)/Rookie"
           checked={filter === 'rookies'}
           onClick={(checked) => setFilter(checked ? 'rookies' : 'none')}
         />
         <Checkbox
-          label='Other'
+          label="Other"
           checked={filter === 'other'}
           onClick={(checked) => setFilter(checked ? 'other' : 'none')}
         />
-        <RemainingPicks noPicksLeft={remainingPicks === 0}>
-          Picks Left: {remainingPicks}
-        </RemainingPicks>
+        <RemainingPicks noPicksLeft={remainingPicks === 0}>Picks Left: {remainingPicks}</RemainingPicks>
       </FiltersContainer>
       <StyledGolfersList disabled={remainingPicks === 0}>
         {results.map((golfer, i) => {
-          return (
-            <GolfersListItem
-              key={i}
-              golfer={golfer}
-              availableView={true}
-              onIconClick={addGolfer}
-            />
-          );
+          return <GolfersListItem key={i} golfer={golfer} availableView={true} onIconClick={addGolfer} />;
         })}
       </StyledGolfersList>
     </GolfersListContainer>
